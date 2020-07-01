d8bb308ae8460d743dab152430a2b1f4e2e45996
Gabrieldevs77 <petollolmel@gmail.com>
2020-07-01 13:31:46 -0300

    ...

--- Produto.cs
+++ Produto.cs
@@ -2,84 +2,120 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Cauculadora_09
 {
     public class Produto
     {
-        
-        public int Codigo { get; set; }
+         public int Codigo { get; set; }
         public string Nome { get; set; }
         public float Preco { get; set; }
 
-        private const string PATH = "Database/produto.csv";
+        private const string PATH = "Database/Produto.csv";
 
         public Produto()
-        {   
+        {
 
-            
-            string pasta = PATH.Split('/')[0];
-            if(!Directory.Exists(pasta))
-            {
-                Directory.CreateDirectory(pasta);
-            }
-
-            // cria se não tiver
-            if(!File.Exists(PATH))
-            {
+            // Solução do desafio e criar os arquivos se n existir
+                if(!File.Exists(PATH))
+                {
+                Directory.CreateDirectory("Database");
                 File.Create(PATH).Close();
-            }
+                }
         }
 
-        public void Cadastrar(Produto prod)
-        {            
-            string[] linha = new string[] { PrepararLinha(prod) };
+        public void Inserir(Produto prod)
+        {
+            var linha = new string[] { PrepararLinha(prod) };
             File.AppendAllLines(PATH, linha);
         }
 
+        /// <summary>
+        /// Metodo paraler os produtos em lista
+        /// </summary>
+        /// <returns>Lista de produtos no arquivo csv e no console</returns>
         public List<Produto> Ler()
         {
-            
+            //lista criada
             List<Produto> prod = new List<Produto>();
 
-            
+            //Transformar as linhas em um array de string;
             string[] linhas = File.ReadAllLines(PATH);
 
-       
-            foreach(string linha in linhas)
+            //Varri o array em strings
+            foreach( var linha in linhas )
             {
-                string[] dado = linha.Split(";");
+                //Separei os termos entre os ';'
+                string[] dado = linha.Split(';');
 
-             
+                // dado[0] = codigo=1
+                // dado[1] = nome=Gibson
+                // dado[2] = preco=5500
 
-                Produto p   = new Produto();
+                //declarando um novo tipo de "produto" e tratando os resultados;
+                Produto p   = new Produto(); 
                 p.Codigo    = Int32.Parse( Separar(dado[0]) );
                 p.Nome      = Separar(dado[1]);
                 p.Preco     = float.Parse( Separar(dado[2]) );
 
+                //Adicionei o produto na lista;
                 prod.Add(p);
             }
 
+            prod = prod.OrderBy( z => z.Nome ).ToList();
+            //Retornei o produto;
             return prod;
         }
 
-        /// <summary>
-        /// Método que separa o símbolo de = da string do csv
-        /// </summary>
-        /// <param name="dado">Coluna do csv separada</param>
-        /// <returns>string somente com o valor da coluna</returns>
-        public string Separar(string dado)
+
+        //filtro de produstos (desafio da aula)
+        public List<Produto> Filtrar(string _nome)
         {
-        
-            return dado.Split("=")[1];
+            return Ler().FindAll(x => x.Nome == _nome);
         }
 
-       
+        public void Remover(string _termo)
+        {
+            //Criamos uma lista de linhas para fazer um "backup"
+            //na memoria do sistema
+            List<string> linhas = new List<string>();
+
+
+            using(StreamReader arquivo = new StreamReader(PATH))
+            {
+                string linha;
+                while((linha = arquivo.ReadLine()) != null)
+                {
+                    linhas.Add(linha);
+                }
+                linhas.RemoveAll(z => z.Contains(_termo));
+            }
+
+            //criamos uma forma de reescrever o arquivo sem as linhas rmovidas
+            using(StreamWriter output = new StreamWriter(PATH))
+            {
+                output.Write(String.Join(Environment.NewLine, linhas.ToArray())); 
+            }
+        }
+
+        /// <summary>
+        /// metodo que separa os termos do "="
+        /// </summary>
+        /// <param name="dados">separa os dados do csv</param>
+        /// <returns>somente os valores da coluna</returns>
+        public string Separar(string dados){
+            //Separei os dados em dois
+            //Antes: código = {p.Codigo};
+            //Agora: código[0] | {p.Codigo}[1];
+            return dados.Split('=')[1];
+
+        }
+
+
         private string PrepararLinha(Produto p)
         {
-            return $"codigo={p.Codigo};nome={p.Nome};preco={p.Preco}";
+            return $"\ncodigo={Codigo};nome={Nome};preco={Preco}";
         }
-
-
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,22 +5,20 @@ namespace Cauculadora_09
 {
     class Program
     {
-         static void Main(string[] args)
-        {
-           
-            Produto p1 = new Produto();
-            p1.Codigo = 2;
-            p1.Nome = "Havaianas";
-            p1.Preco = 26f;
+        Produto p1 = new Produto();
+           p1.Codigo = 1;
+           p1.Nome = "Gabinete gamer";
+           p1.Preco = 2029.99f;
 
-            p1.Cadastrar(p1);
+           p1.Inserir(p1);
+           p1.Remover("carro");
 
-            List<Produto> csv = p1.Ler();
+           List<Produto> lista = p1.Ler();
 
-            foreach (Produto item in csv)
-            {
-                Console.WriteLine($"R$ {item.Preco} - {item.Nome}");
-            }
-        }
+       
+           foreach(Produto item in lista)
+           {
+               System.Console.WriteLine($"R${item.Preco} - {item.Nome}");
+           }
     }
 }
