c7c982e798ea7e568536999741b73686b188901f
Gabrieldevs77 <petollolmel@gmail.com>
2020-06-30 23:40:35 -0300

    fim

--- Produto.cs
+++ Produto.cs
@@ -1,10 +1,13 @@
 
+using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Cauculadora_09
 {
     public class Produto
     {
+        
         public int Codigo { get; set; }
         public string Nome { get; set; }
         public float Preco { get; set; }
@@ -12,8 +15,16 @@ namespace Cauculadora_09
         private const string PATH = "Database/produto.csv";
 
         public Produto()
-        {         
-            // Cria o arquivo caso ele seja inexistente
+        {   
+
+            
+            string pasta = PATH.Split('/')[0];
+            if(!Directory.Exists(pasta))
+            {
+                Directory.CreateDirectory(pasta);
+            }
+
+            // cria se não tiver
             if(!File.Exists(PATH))
             {
                 File.Create(PATH).Close();
@@ -26,11 +37,49 @@ namespace Cauculadora_09
             File.AppendAllLines(PATH, linha);
         }
 
+        public List<Produto> Ler()
+        {
+            
+            List<Produto> prod = new List<Produto>();
 
+            
+            string[] linhas = File.ReadAllLines(PATH);
+
+       
+            foreach(string linha in linhas)
+            {
+                string[] dado = linha.Split(";");
+
+             
+
+                Produto p   = new Produto();
+                p.Codigo    = Int32.Parse( Separar(dado[0]) );
+                p.Nome      = Separar(dado[1]);
+                p.Preco     = float.Parse( Separar(dado[2]) );
+
+                prod.Add(p);
+            }
+
+            return prod;
+        }
+
+        /// <summary>
+        /// Método que separa o símbolo de = da string do csv
+        /// </summary>
+        /// <param name="dado">Coluna do csv separada</param>
+        /// <returns>string somente com o valor da coluna</returns>
+        public string Separar(string dado)
+        {
+        
+            return dado.Split("=")[1];
+        }
+
+       
         private string PrepararLinha(Produto p)
         {
             return $"codigo={p.Codigo};nome={p.Nome};preco={p.Preco}";
         }
+
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,17 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Cauculadora_09
 {
     class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
-            Produto p0 = new Produto();
-            p0.Codigo = 0;
-            p0.Nome = "Havaianas";
-            p0.Preco = 55f;
+           
+            Produto p1 = new Produto();
+            p1.Codigo = 2;
+            p1.Nome = "Havaianas";
+            p1.Preco = 26f;
 
-            p0.Cadastrar(p0);
+            p1.Cadastrar(p1);
+
+            List<Produto> csv = p1.Ler();
+
+            foreach (Produto item in csv)
+            {
+                Console.WriteLine($"R$ {item.Preco} - {item.Nome}");
+            }
         }
     }
 }
